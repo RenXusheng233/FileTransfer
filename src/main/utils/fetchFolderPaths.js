65102cb63a54df426dfd313c4e13ea5df058dd4e
renxusheng <782449909@qq.com>
2019-07-31 09:28:01 +0800

    代码优化.

--- src/main/utils/fetchFolderPaths.js
+++ src/main/utils/fetchFolderPaths.js
@@ -1,5 +1,4 @@
 import Store from 'electron-store'
-import uuidv4 from 'uuid/v4'
 
 // 这里如果使用 extends 会报错
 // 内部检查, 继承时无 new 实例则报错
@@ -22,17 +21,7 @@ class DataStore {
 
   // 从模态窗口传过来的文件夹路径数组
   addFolders(folders) {
-    const foldersWithIds = folders.map((folder) => {
-      return {
-        id: uuidv4(),
-        path: folder.path,
-      }
-    })
-    this.folders = foldersWithIds
-    return this.saveFolders()
-  }
-  deleteFolders(deletedId) {
-    this.folders = this.folders.filter((folder) => folder.id !== deletedId)
+    this.folders = folders
     return this.saveFolders()
   }
 }
