4d647428d1e2ca00f477b9fab2fdecc20638676f
renxusheng <782449909@qq.com>
2019-08-01 16:09:16 +0800

    chore: 完成本地路径树的渲染 [延时赋值强制渲染].

--- src/renderer/components/localPath.vue
+++ src/renderer/components/localPath.vue
@@ -4,9 +4,10 @@
 
     <el-tree
       class="filter-tree"
-      :data="pathsInfoTree"
+      :data="localPaths"
       :props="defaultProps"
       :filter-node-method="filterNode"
+      @node-click="handleNodeClick"
       ref="tree"
     ></el-tree>
   </div>
@@ -24,22 +25,42 @@ export default {
   data() {
     return {
       filterText: '', // 节点过滤文本
+      localPaths: [],
       defaultProps: {
         children: 'children',
         label: 'path',
       },
     }
   },
+  mounted() {
+    this.localPaths = this.pathsInfoTree
+  },
   watch: {
     filterText(val) {
       this.$refs.tree.filter(val)
+    },
+    pathsInfoTree(val) {
+      /**
+       * 先将数据置为空
+       * 然后通过延时赋值强制重新渲染
+       * 如果不做此操作路径树的渲染有问题
+       */
+      this.localPaths = []
+      setTimeout(() => {
+        this.localPaths = val
+      }, 20)
     },
   },
   methods: {
     // 本地路径 - 节点过滤
     filterNode(value, data) {
       if (!value) return true
-      return data.label.indexOf(value) !== -1
+      return data.path.indexOf(value) !== -1
+    },
+
+    // 点击节点事件
+    handleNodeClick(data) {
+      console.log(data)
     },
   },
 }
