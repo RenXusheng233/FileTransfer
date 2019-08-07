fdfba8202297c07a9fe72ce7c129b6796b082895
renxusheng <782449909@qq.com>
2019-08-07 11:47:07 +0800

    chore: 根据获取的本地路径展示其详情到表格中.

--- src/renderer/pages/landingPage/modules/localPathDetail.vue
+++ src/renderer/pages/landingPage/modules/localPathDetail.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="local-path-detail-wrapper">
-    <el-table :data="detailTable" stripe style="width: 100%">
+    <el-table
+      :data="detailTable"
+      size="small"
+      :highlight-current-row="true"
+      style="width: 100%"
+    >
       <el-table-column align="center" prop="fileName" label="文件名"></el-table-column>
       <el-table-column align="center" prop="fileSize" label="文件大小"></el-table-column>
       <el-table-column align="center" prop="fileType" label="文件类型"></el-table-column>
@@ -13,23 +18,11 @@
 import { mapState } from 'vuex'
 import fs from 'fs'
 import path from 'path'
+import moment from 'moment'
 export default {
   data() {
     return {
-      detailTable: [
-        {
-          fileName: 'hello',
-          fileSize: '1kb',
-          fileType: 'txt',
-          modifyTime: 'xxx.xxx',
-        },
-        {
-          fileName: 'world',
-          fileSize: '2kb',
-          fileType: 'txt',
-          modifyTime: 'ccc.ccc',
-        },
-      ],
+      detailTable: [],
     }
   },
   computed: {
@@ -46,6 +39,7 @@ export default {
   methods: {
     // 根据路径遍历
     ergodicFile(localPath) {
+      this.detailTable = [] // 点击切换选中文件夹时先清空详情
       fs.readdir(localPath, (err, files) => {
         if (err) {
           console.warn(err)
@@ -61,12 +55,28 @@ export default {
 
                 // 如果是文件夹, 则获取该文件夹名, 文件夹大小, 修改时间
                 if (isDerectory) {
-                  console.log('Folder =====', filedir)
+                  const obj = {
+                    fileName: filename,
+                    fileSize: stats.size,
+                    fileType: '文件夹',
+                    modifyTime: moment(stats.mtime).format(
+                      'YYYY-MM-DD HH:mm:ss'
+                    ),
+                  }
+                  this.detailTable.push(obj)
                 }
 
                 // 如果是文件, 则需获取文件名, 文件大小, 文件类型, 修改时间
                 if (isFile) {
-                  console.log('File *****', filedir)
+                  const obj = {
+                    fileName: filename,
+                    fileSize: stats.size,
+                    fileType: path.extname(filename),
+                    modifyTime: moment(stats.mtime).format(
+                      'YYYY-MM-DD HH:mm:ss'
+                    ),
+                  }
+                  this.detailTable.push(obj)
                 }
               }
             })
